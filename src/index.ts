3aa4c8434d9ca31c7506b864bb685a6db71bc1b4
César Brie <ces@ces.rocks>
2020-04-11 03:40:39 -0300

    export SourcedAction

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { createReducer } from './create-reducer.function';
-export { createSourcedAction } from './create-sourced-action.function';
-export { isSourcedAction } from './create-sourced-action.function';
+export { createSourcedAction, isSourcedAction, SourcedAction } from './create-sourced-action.function';
 export { ofType } from './of-type.operator';
